c568a87273d1f6d28d6db98da822e222fc6efe95
Elias Fink <mail@eliasfink.de>
2024-05-18 04:02:49 +0200

    Blabla

--- src/main/java/logic/Strategy.java
+++ src/main/java/logic/Strategy.java
@@ -9,10 +9,13 @@ import models.BoardAction;
 import models.GameState;
 import models.PlayerAction;
 
+import static java.lang.Math.*;
+
 public class Strategy {
     
     public static List<PlayerAction> decide(GameState gameState) {
         int myId = gameState.game.player;
+        List<Base> allBases = gameState.bases;
         List<Base> myBases = gameState.bases.stream().filter(base -> base.player == myId).toList();
         List<Base> foreignBases = gameState.bases.stream().filter(base -> base.player != myId).toList();
         List<Base> emptyBases = gameState.bases.stream().filter(base -> base.player == 0).toList();
@@ -27,17 +30,11 @@ public class Strategy {
         playerActions = takeEmptyBases(myBases, emptyBases, playerActions);
         System.out.println("player actions: " + playerActions);
 
-        List<BoardAction> attacksOnMyBases = gameState.actions.stream().filter(action -> {
-            AtomicBoolean isOnMe = new AtomicBoolean(false);
-            myBases.forEach(base -> {
-                isOnMe.set(base.uid == action.dest || isOnMe.get());
-            });
-            return isOnMe.get();
-        }).toList();
 
-        for (BoardAction attack: attacksOnMyBases) {
-            System.out.println("attack on one of my bases incoming: " + attack);
-        }
+
+        //for (BoardAction attack: attacksOnMyBases) {
+        //    System.out.println("attack on one of my bases incoming: " + attack);
+        //}
 
         return playerActions;
     }
@@ -59,6 +56,65 @@ public class Strategy {
         }
 
         return playerActions;
+
+    }
+
+
+    public static List<PlayerAction> abc(GameState gameState, List<Base> allBases, List<Base> myBases, List<PlayerAction> playerActions) {
+
+        List<BoardAction> attacksOnMe = gameState.actions.stream().filter(action -> {
+            AtomicBoolean isTargetingMe = new AtomicBoolean(false);
+            myBases.forEach(base -> {
+                isTargetingMe.set(base.uid == action.dest || isTargetingMe.get());
+            });
+            return isTargetingMe.get();
+        }).toList();
+
+        for (BoardAction attack: attacksOnMe) {
+            Base target = getBaseById(attack.dest, allBases);
+            if (attack.amount > target.population) {
+                // TODO: && attack.progress.distance < ...
+                Base sourceBase = getNearestBase(target, myBases);
+                playerActions.add(new PlayerAction(sourceBase.uid, target.uid, target.population + 1));
+            }
+        }
+
+        return playerActions;
+
+    }
+
+
+    public static double getDistance(Base base1, Base base2) {
+
+        return sqrt( pow(base1.position.x - base2.position.x, 2) + pow(base1.position.y - base2.position.y, 2) + pow(base1.position.z - base2.position.z, 2) );
+
+    }
+
+
+    public static Base getBaseById(int id, List<Base> allBases) {
+
+        for (Base base: allBases) {
+            if (base.uid == id) {
+                return base;
+            }
+        }
+        return null;
+
+    }
+
+
+    public static Base getNearestBase(Base base1, List<Base> otherBases) {
+
+        double min = 999999;
+        Base nearestBase = null;
+        for (Base base: otherBases) {
+            double distance = getDistance(base1, base);
+            if (distance < min) {
+                min = distance;
+                nearestBase = base1;
+            }
+        }
+        return nearestBase;
 
     }
 
